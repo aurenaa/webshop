dfdead003f8a2331fa6073b1278323f8cf022157
aurenaa <salapuranev@gmail.com>
2025-09-19 11:25:43 +0200

    buyer and seller profiles finished, missing rating and report

--- frontend/src/pages/UserProfilePage/UserProfilePage.js
+++ frontend/src/pages/UserProfilePage/UserProfilePage.js
@@ -10,9 +10,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "./UserProfilePage.css";
 
 export default function UserProfilePage() {
-    const { userLoggedIn } = useUser();
+    const { user, userLoggedIn } = useUser();
     const navigate = useNavigate();
-    const { isLoggedIn } = useAuthorize();
+    const { isLoggedIn, setIsLoggedIn } = useAuthorize();
     const [userProfile, setUserProfile] = useState(null);
     const { id } = useParams();
     const [product] = useState(null);
@@ -30,8 +30,20 @@ export default function UserProfilePage() {
     }, [id, isLoggedIn, userLoggedIn]);
 
     const userProducts = useMemo(() => {
-        return userProfile ? products.filter(p => p.sellerId === userProfile.id) : [];
+        if (!userProfile) return [];
+        if (!userProfile.productList) return [];
+
+        const productIds = typeof userProfile.productList === "string"
+            ? userProfile.productList.split("|").map(id => Number(id))
+            : userProfile.productList;
+
+        return products.filter(p => productIds.includes(p.id));
     }, [userProfile, products]);
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+        navigate('/mainpage');
+    };
 
     return (
         <div className="main-page">
@@ -46,8 +58,36 @@ export default function UserProfilePage() {
                     />
                     <button className="btn btn-outline-success" type="submit">Search</button>
                 </div>
-            </nav>
 
+                <div className="d-flex align-items-center ms-auto">
+                <button onClick={() => navigate("/add-product")} className="btn btn-success me-2">Add a Listing</button>
+                    {isLoggedIn ? (
+                    <>
+                        <img className="cart" src="/icons/shopping_cart.png" alt="Cart" onClick={() => navigate("/cart")}/>
+                        <div className="dropdown">
+                            <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img className="menu" src="/icons/menu.png" alt="Menu"/>
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <li><a className="dropdown-item" onClick={() => navigate(`/profile/${user.id}`)}>My account</a></li>
+                            <li><a className="dropdown-item" onClick={() => navigate("/listingpage")}>My listings</a></li>
+                            <li><a className="dropdown-item" onClick={handleLogout}>Log out</a></li>
+                            </ul>
+                        </div>
+                    </>
+                    ) : (
+                    <>
+                        <button onClick={() => navigate("/signup")} className="btn btn-outline-primary me-2">
+                        Sign Up
+                        </button>
+                        <span onClick={() => navigate("/login")} className="nav-link" style={{ cursor: "pointer" }}>
+                        Log in
+                        </span>
+                    </>
+                    )}
+                </div>
+            </nav>
+            
             {userProfile && (
                 <>
                     {userProfile.role === "SELLER" && (
@@ -56,11 +96,12 @@ export default function UserProfilePage() {
                                 <img className="user-img"
                                      src={userProfile?.profileImage ? `http://localhost:8080/WebShopAppREST/images/profiles/${userProfile.profileImage}` : "/icons/account_circle.png"}
                                 />
-                                <div
-                                    className="username"
-                                    onClick={() => userProfile?.id === product?.sellerId ? navigate("/profile") : navigate(`/user/${product?.sellerId}`)}
-                                >
+                                <div className="username">
                                     {userProfile.username}
+                                </div>
+                                <div className="rating">
+                                    <div>Average rating:</div>
+                                    <div>4.7</div>
                                 </div>
                             </div>
                             <div className="profile-details">
@@ -74,11 +115,11 @@ export default function UserProfilePage() {
                                     Feedback
                                 </div>                  
 
-                                <nav className="navbar bg-body-tertiary">
+                                <nav className="navbar bg-body">
                                     <div className="container-fluid">
                                         <form className="d-flex" role="search">
-                                            <input className="form-control me-2" type="search" placeholder={`Search all ${userProfile?.productList?.length || 0} items`} aria-label="Search"/>
-                                            <button className="btn btn-outline-dark" type="submit">
+                                            <input className="form-control me-2 search" type="search" placeholder={`Search all ${userProfile?.productList?.length || 0} items`} aria-label="Search"/>
+                                            <button className="btn" type="submit">
                                                 <img className="search-img" src="/icons/search.png"/>
                                             </button>
                                         </form>
@@ -105,8 +146,11 @@ export default function UserProfilePage() {
                                 )}
 
                             {activeTab === "about" && (
-                                <div>
-                                <p>{userProfile?.description || "No information provided."}</p>
+                                <div className="about-section">
+                                    <p className="section">Description:</p>
+                                    <p>{userProfile?.description || "No information provided."}</p>
+                                    <p className="section">Email:</p>
+                                    <p>{userProfile?.email || "No information provided."}</p>
                                 </div>
                             )}
 
@@ -122,8 +166,66 @@ export default function UserProfilePage() {
                     )}
 
                     {userProfile.role === "BUYER" && (
-                        <div>
+                        <div className="background">
+                            <div className="header">
+                                <img className="user-img"
+                                     src={userProfile?.profileImage ? `http://localhost:8080/WebShopAppREST/images/profiles/${userProfile.profileImage}` : "/icons/account_circle.png"}
+                                />
+                                <div className="username">
+                                    {userProfile.username}
+                                </div>
+                                <div className="rating">
+                                    <div>Average rating:</div>
+                                    <div>4.7</div>
+                                </div>
+                            </div>
+                            <div className="profile-details">
+                                <div className={`profile-click ${activeTab === "items" ? "active" : ""}`} onClick={() => setActiveTab("items")}>
+                                    Purchases ({userProfile?.productList?.length || 0})
+                                </div>
+                                <div className={`profile-click ${activeTab === "about" ? "active" : ""}`} onClick={() => setActiveTab("about")}>
+                                    About
+                                </div>
+                                <div className={`profile-click ${activeTab === "feedback" ? "active" : ""}`} onClick={() => setActiveTab("feedback")}>
+                                    Feedback
+                                </div>                  
+                            </div>
+                            <div className="profile-content mt-3 tabel">
+                                {activeTab === "items" && (
+                                <div>
+                                    {userProducts.length > 0 ? (
+                                        Array.from({ length: Math.ceil(userProducts.length / 3) }).map((_, rowIndex) => (
+                                            <div className="row row-cols-auto mb-3" key={rowIndex}>
+                                                {userProducts.slice(rowIndex * 3, rowIndex * 3 + 3).map((product) => (
+                                                    <div className="col" key={product.id}>
+                                                    <ProductTable products={[product]} />
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        ))
+                                            ) : (
+                                            <p>No items to display.</p>
+                                    )}
+                                </div>
+                                )}
 
+                            {activeTab === "about" && (
+                                <div className="about-section">
+                                    <p className="section">Description:</p>
+                                    <p>{userProfile?.description || "No information provided."}</p>
+                                    <p className="section">Email:</p>
+                                    <p>{userProfile?.email || "No information provided."}</p>
+                                </div>
+                            )}
+
+                            {activeTab === "feedback" && (
+                                <div>
+                                {userProfile?.feedback?.map(f => (
+                                    <div key={f.id}>{f.comment}</div>
+                                ))}
+                                </div>
+                            )}
+                            </div>
                         </div>
                     )}
                 </>
